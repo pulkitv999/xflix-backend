7838471581a2b05fe05a4a133795abb07f8ac6f2
CrioUser <crio-user@criodo.com>
2023-08-02 22:06:48 +0530

    ME_BUILDOUT_XFLIX_NODE_MODULE_NODE

---
+++ setupRemoteMongo.sh
@@ -0,0 +1,1 @@
+mongoimport --uri "mongodb://ac-wts67pl-shard-00-00.xgbcmt1.mongodb.net:27017,ac-wts67pl-shard-00-01.xgbcmt1.mongodb.net:27017,ac-wts67pl-shard-00-02.xgbcmt1.mongodb.net:27017/?replicaSet=atlas-8f7x2q-shard-0" --ssl --authenticationDatabase admin --username xflix --password EkKzBnwn --drop --collection videos --file data.json
